1bb5fd13ed3dd04540976af846159191e5c2ac80
sungchuni <new.sungchuni@gmail.com>
2020-11-26 23:21:33 +0900

    (feat) add fullscreen shortcut

--- listenFullscreen.js
+++ listenFullscreen.js
@@ -5,4 +5,10 @@ export default function listenFullscreen(unityInstance) {
       Object.assign(document.createElement("div"), {className: "fullscreen"})
     )
     .addEventListener("click", () => unityInstance.SetFullscreen(1));
+  document.addEventListener(
+    "keyup",
+    ({code, key}) =>
+      (code === "KeyF" || ["f", "F"].includes(key)) &&
+      unityInstance.SetFullscreen(document.fullscreenElement ? 0 : 1)
+  );
 }
